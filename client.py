59489658bd9354f34d4ad2bfa9fefff87c94603f
Leon Cai <cai.855@buckeyemail.osu.edu>
2022-04-23 20:35:05 -0400

    Finishe client

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import json
 import sys
 
 # Connecting To Server
-def connect(self, address, port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(address, port)
-    return client
+# def connect(address, port):
+#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client.connect(address, port)
+#     return client
 # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # client.connect(('127.0.0.1', 55555))
 def toJsonString(name, input):
@@ -35,10 +35,22 @@ def receive(client, name):
             client.close()
             break
 # Sending Messages To Server
-def write(client, name):
-    while True:
-        message = '{}: {}'.format(name, toJsonString(name, input('')))#toJsonString(name, input('')
-        client.send(message.encode('ascii'))
+# def write(client, name):
+#     while True:
+#         try:
+#             command = input('')
+#             message = '{}: {}'.format(name, toJsonString(name, command))
+#             client.send(message.encode('ascii'))
+        # except:
+        #     # Close Connection When Error
+        #     print("An error occured!")
+        #     client.close()
+        #     break    
+        # message =  input(f'{name} > ')
+        # if message:
+        #     message = message.encode('utf-8')
+        #     message_header = f"{len(message):<{10}}".encode('utf-8')
+        #     client.send(message_header + message)
 # Starting Threads For Listening And Writing
 
 username = input("Give a username: ")
@@ -50,12 +62,18 @@ if arguments[0] == 'connect':
         print("Connect needs two arguments: address, port ")
         sys.exit()
     # connect to the client
-    client_socket =connect(arguments[1], arguments[2])
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((arguments[1], int(arguments[2])))
     # Starting Threads For Listening And Writing
     receive_thread = threading.Thread(target=receive(client_socket,username))
-
-    write_thread = threading.Thread(target=write(client_socket, username))
-    write_thread.start()
+    receive_thread.start()
+    print('gggggg')
+    while True:
+        command = input('')
+        message = '{}: {}'.format(username, toJsonString(username, command))
+        client_socket.send(message.encode('ascii'))
+        # write_thread = threading.Thread(target=write(client_socket, username))
+        # write_thread.start()
 else:
     print('Error: Need to connect to the server first!')
     sys.exit()
