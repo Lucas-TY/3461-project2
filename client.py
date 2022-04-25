6797f007acbca95d18d608416b562b23ad98a3fa
子恒 蔡 <czh201630712@gmail.com>
2022-04-25 15:34:19 -0400

    changename

--- client.py
+++ client.py
@@ -5,9 +5,13 @@ import json
 import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
+
+
+#  This is the chat client program.
+#  @author Lucas Wu/Leon Cai
 class Client:
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # ask for user name
         msg = tkinter.Tk()
         msg.withdraw()
@@ -18,50 +22,51 @@ class Client:
         self.arguments = simpledialog.askstring('connect', 'Please connect to server using %' +'connect', parent = msg)
         #connect to server
         self.command = self.arguments.split()
-        self.sock.connect((self.command[1],int(self.command[2])))
-        self.gui_done = False
+        self.client_socket.connect((self.command[1],int(self.command[2])))
+        self.isDone = False
         self.running = True
-        gui_thread = threading.Thread(target = self.gui_loop)
+        gui_thread = threading.Thread(target = self.gui_iteration)
         receive_thread = threading.Thread(target = self.receive)
 
         gui_thread.start()
         receive_thread.start()
     
-    def gui_loop(self):
-        self.win = tkinter.Tk()
-        self.win.configure(bg='lightgray')
+    def gui_iteration(self):
+        self.window = tkinter.Tk()
+        self.window.configure(bg='lightgray')
 
-        self.chat_label = tkinter.Label(self.win, text = 'Chat:', bg='lightgray')
+        self.chat_label = tkinter.Label(self.window, text = 'Chat:', bg='lightgray')
         self.chat_label.config(font=('Arial', 12))
         self.chat_label.pack(padx=20, pady=5)
 
-        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.window)
         self.text_area.pack(padx =20, pady=5)
         self.text_area.config(state='disabled')
 
-        self.msg_label = tkinter.Label(self.win, text = 'Message:', bg='lightgray')
+        self.msg_label = tkinter.Label(self.window, text = 'Message:', bg='lightgray')
         self.msg_label.config(font=('Arial', 12))
         self.msg_label.pack(padx=20, pady=5)
 
-        self.input_area = tkinter.Text(self.win, height = 3)
+        self.input_area = tkinter.Text(self.window, height = 3)
         self.input_area.pack(padx=20, pady=5)
-        self.send_button = tkinter.Button(self.win, text = 'Send', command = self.write)
+        self.send_button = tkinter.Button(self.window, text = 'Send', command = self.write)
         self.send_button.config(font=('Arial', 12))
         self.send_button.pack(padx=20, pady=5)
-        self.gui_done = True
-        self.win.protocol('WM_DELETE_WINDOW', self.stop)
-        self.win.mainloop()
+        self.isDone = True
+        self.window.protocol('WM_DELETE_WINDOW', self.stop)
+        self.window.mainloop()
 
     def write(self):
         message = '{}'.format(self.toJsonString(self.username, self.input_area.get('1.0','end-1c')))
-        self.sock.send(message.encode('ascii'))
+        self.client_socket.send(message.encode('ascii'))
         self.input_area.delete('1.0', 'end')
+
     def receive(self):
        while self.running:
             try:
-                message = self.sock.recv(1024).decode('ascii')
+                message = self.client_socket.recv(1024).decode('ascii')
                 # Receive Message From Server
-                if self.gui_done:
+                if self.isDone:
                     self.text_area.config(state = 'normal')
                     self.text_area.insert('end', message)
                     self.text_area.yview('end')
@@ -69,13 +74,15 @@ class Client:
             except:
                 # Close Connection When Error
                 print("An error occured!")
-                self.sock.close()
+                self.client_socket.close()
                 break
+
     def stop(self):
         self.running = False
-        self.win.destroy()
-        self.sock.close()
+        self.window.destroy()
+        self.client_socket.close()
         exit(0)
+
     # change commands to jason string
     def toJsonString(self, name, input):
         arguments = input.split(';')
